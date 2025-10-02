96f5f41ca1d0b5cbdf2d5d74b44e708325f3ca59
julianchavarro <jasi90111@gmail.com>
2025-10-02 17:52:00 -0500

    Cambio base de datos

--- adsofitness/settings.py
+++ adsofitness/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'adsofitness.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adsofitness',
+        'USER': 'adso_user',
+        'PASSWORD': 'NuevaContrasenaSegura123',
+        'HOST': '127.0.0.1',  # <-- Cambia localhost por 127.0.0.1
+        'PORT': '3306',
     }
 }
 
